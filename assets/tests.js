6225169065130d0b733958c6d46af288ca218d10
Per Rosendahl <per.rosendahl@sweco.se>
2017-04-26 22:18:30 +0200

    publish: 1.3.0
    generated from commit 51999c32cb16be8cb741a6b6a6eb7b8864e3f641

--- assets/tests.js
+++ assets/tests.js
@@ -1,84 +1,84 @@
 'use strict';
 
-define('sweco-bootstrap/tests/app.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/app.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | app.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - app.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'app.js should pass jshint.');
+    assert.ok(true, 'app.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/bootstrap-modal.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/bootstrap-modal.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/bootstrap-modal.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/bootstrap-modal.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/bootstrap-modal.js should pass jshint.');
+    assert.ok(true, 'components/bootstrap-modal.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/progress-js.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/progress-js.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/progress-js.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/progress-js.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/progress-js.js should pass jshint.');
+    assert.ok(true, 'components/progress-js.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/sweco-formgroup.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/sweco-formgroup.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/sweco-formgroup.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/sweco-formgroup.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/sweco-formgroup.js should pass jshint.');
+    assert.ok(true, 'components/sweco-formgroup.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/sweco-input.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/sweco-input.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/sweco-input.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/sweco-input.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/sweco-input.js should pass jshint.');
+    assert.ok(true, 'components/sweco-input.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/sweco-label.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/sweco-label.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/sweco-label.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/sweco-label.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/sweco-label.js should pass jshint.');
+    assert.ok(true, 'components/sweco-label.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/components/sweco-search.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/components/sweco-search.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | components/sweco-search.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - components/sweco-search.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'components/sweco-search.js should pass jshint.');
+    assert.ok(true, 'components/sweco-search.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/controllers/application.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/controllers/application.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | controllers/application.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - controllers/application.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'controllers/application.js should pass jshint.');
+    assert.ok(true, 'controllers/application.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/controllers/index.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/controllers/index.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | controllers/index.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - controllers/index.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'controllers/index.js should pass jshint.');
+    assert.ok(true, 'controllers/index.js should pass ESLint.\n');
   });
 });
 define('sweco-bootstrap/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
@@ -88,13 +88,13 @@ define('sweco-bootstrap/tests/helpers/destroy-app', ['exports', 'ember'], functi
     _ember['default'].run(application, 'destroy');
   }
 });
-define('sweco-bootstrap/tests/helpers/destroy-app.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/helpers/destroy-app.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | helpers/destroy-app.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - helpers/destroy-app.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint.\n');
   });
 });
 define('sweco-bootstrap/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'sweco-bootstrap/tests/helpers/start-app', 'sweco-bootstrap/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _swecoBootstrapTestsHelpersStartApp, _swecoBootstrapTestsHelpersDestroyApp) {
@@ -123,13 +123,13 @@ define('sweco-bootstrap/tests/helpers/module-for-acceptance', ['exports', 'qunit
     });
   };
 });
-define('sweco-bootstrap/tests/helpers/module-for-acceptance.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/helpers/module-for-acceptance.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | helpers/module-for-acceptance.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - helpers/module-for-acceptance.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint.\n');
   });
 });
 define('sweco-bootstrap/tests/helpers/resolver', ['exports', 'sweco-bootstrap/resolver', 'sweco-bootstrap/config/environment'], function (exports, _swecoBootstrapResolver, _swecoBootstrapConfigEnvironment) {
@@ -143,13 +143,13 @@ define('sweco-bootstrap/tests/helpers/resolver', ['exports', 'sweco-bootstrap/re
 
   exports['default'] = resolver;
 });
-define('sweco-bootstrap/tests/helpers/resolver.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/helpers/resolver.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | helpers/resolver.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - helpers/resolver.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+    assert.ok(true, 'helpers/resolver.js should pass ESLint.\n');
   });
 });
 define('sweco-bootstrap/tests/helpers/start-app', ['exports', 'ember', 'sweco-bootstrap/app', 'sweco-bootstrap/config/environment'], function (exports, _ember, _swecoBootstrapApp, _swecoBootstrapConfigEnvironment) {
@@ -170,59 +170,55 @@ define('sweco-bootstrap/tests/helpers/start-app', ['exports', 'ember', 'sweco-bo
     return application;
   }
 });
-define('sweco-bootstrap/tests/helpers/start-app.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/helpers/start-app.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | helpers/start-app.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - helpers/start-app.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+    assert.ok(true, 'helpers/start-app.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/resolver.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/resolver.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | resolver.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - resolver.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'resolver.js should pass jshint.');
+    assert.ok(true, 'resolver.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/router.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/router.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | router.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - router.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'router.js should pass jshint.');
+    assert.ok(true, 'router.js should pass ESLint.\n');
   });
 });
 define('sweco-bootstrap/tests/test-helper', ['exports', 'sweco-bootstrap/tests/helpers/resolver', 'ember-qunit'], function (exports, _swecoBootstrapTestsHelpersResolver, _emberQunit) {
 
   (0, _emberQunit.setResolver)(_swecoBootstrapTestsHelpersResolver['default']);
 });
-define('sweco-bootstrap/tests/test-helper.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/test-helper.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | test-helper.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - test-helper.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'test-helper.js should pass jshint.');
+    assert.ok(true, 'test-helper.js should pass ESLint.\n');
   });
 });
-define('sweco-bootstrap/tests/transitions.jshint', ['exports'], function (exports) {
+define('sweco-bootstrap/tests/transitions.lint-test', ['exports'], function (exports) {
   'use strict';
 
-  QUnit.module('JSHint | transitions.js');
-  QUnit.test('should pass jshint', function (assert) {
+  QUnit.module('ESLint - transitions.js');
+  QUnit.test('should pass ESLint', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'transitions.js should pass jshint.');
+    assert.ok(true, 'transitions.js should pass ESLint.\n');
   });
 });
-/* jshint ignore:start */
-
 require('sweco-bootstrap/tests/test-helper');
 EmberENV.TESTS_FILE_LOADED = true;
-
-/* jshint ignore:end */
 //# sourceMappingURL=tests.map
