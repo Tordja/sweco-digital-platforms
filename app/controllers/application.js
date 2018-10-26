1617ec80277513c9f7b14b071d83a86db2a7db8b
Per Rosendahl <per.rosendahl@sweco.se>
2018-10-26 14:38:22 +0200

    4.1.7

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -11,6 +11,10 @@ export default Ember.Controller.extend({
     // Tooltip
     $(function () {
       $('[data-toggle="tooltip"]').tooltip();
+      $('.data-toggle-tooltip').tooltip();
+      $('.data-toggle-tooltip').on('click', function(){
+        $('.data-toggle-tooltip').tooltip('hide')
+      });
     });
 
     // Popover
@@ -81,6 +85,30 @@ export default Ember.Controller.extend({
               if( log ) alert(log);
             }
         });
+      });
+    });
+
+    // Dropdown-menu multilevel
+    $(function () {
+      $('.dropdown-menu-multilevel').on('click', function(event){
+        var events = $._data(document, 'events') || {};
+        events = events.click || [];
+        for(var i = 0; i < events.length; i++) {
+            if(events[i].selector) {
+    
+                //Check if the clicked element matches the event selector
+                if($(event.target).is(events[i].selector)) {
+                    events[i].handler.call(event.target, event);
+                }
+    
+                // Check if any of the clicked element parents matches the 
+                // delegated event selector (Emulating propagation)
+                $(event.target).parents(events[i].selector).each(function(){
+                    events[i].handler.call(this, event);
+                });
+            }
+        }
+        event.stopPropagation(); //Always stop propagation
       });
     });
 
